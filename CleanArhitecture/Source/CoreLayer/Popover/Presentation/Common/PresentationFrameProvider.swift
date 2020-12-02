39d067a8a0f68d644fb6e9be4be7ad2c5dbd20d4
Pavel Kochenda <peeoner174@gmail.com>
2020-12-02 20:15:22 +0500

    refactor

--- CleanArhitecture/Source/CoreLayer/Popover/Presentation/Common/PresentationFrameProvider.swift
+++ CleanArhitecture/Source/CoreLayer/Popover/Presentation/Common/PresentationFrameProvider.swift
@@ -20,73 +20,32 @@ protocol PresentationFrameProvider {
 
 typealias FrameOfExpandablePresentedViewClosure = ((_ containerViewFrame: CGRect, _ expandStep: UInt8) throws -> CGRect)?
 
+typealias OnMakeStepClosure = ((_ isMaked: Bool) -> Void)
+
+typealias MakeExpandStepClosure = ((_ containerViewFrame: CGRect, _ expandStep: UInt8, _ onMakeStepClosure: OnMakeStepClosure) throws -> CGRect)?
+
+
 class ExpandablePopoverFrameMeta {
-    internal init(expandSteps: [CGRect] = [], currentExpandStep: UInt8 = 0, blockDismissOnPanGesture: Bool = true, tweakExpandableFrameCommands: [TweakPopoverCommand] = []) {
+    var tweakExpandableFrameCommands: [TweakPopoverCommand]
+    var expandSteps: [CGRect] = []
+    var currentExpandStep: UInt8 = 0
+    var blockDismissOnPanGesture: Bool = true
+    
+    internal init(
+        expandSteps: [CGRect] = [],
+        currentExpandStep: UInt8 = 0,
+        blockDismissOnPanGesture: Bool = true,
+        tweakExpandableFrameCommands: [TweakPopoverCommand] = []
+    ) {
         self.expandSteps = expandSteps
         self.currentExpandStep = currentExpandStep
         self.blockDismissOnPanGesture = blockDismissOnPanGesture
         self.tweakExpandableFrameCommands = tweakExpandableFrameCommands
     }
-    
-    var tweakExpandableFrameCommands: [TweakPopoverCommand]
-    var expandSteps: [CGRect] = []
-    var currentExpandStep: UInt8 = 0
-    var blockDismissOnPanGesture: Bool = true
 }
 
 protocol PresentationExpandableFrameProvider {
     var expandablePopoverFrameMeta: ExpandablePopoverFrameMeta { get set }
     var frameOfExpandablePresentedViewClosure: FrameOfExpandablePresentedViewClosure { get set }
     func getMaximumExpandFrameHeight(forContainerView containerView: UIView) -> CGFloat
-}
-
-
-class WeakSet<T: AnyObject>: Sequence, ExpressibleByArrayLiteral, CustomStringConvertible, CustomDebugStringConvertible {
-
-   private var objects = NSHashTable<T>.weakObjects()
-
-   public init(_ objects: [T]) {
-       for object in objects {
-           insert(object)
-       }
-   }
-
-   public convenience required init(arrayLiteral elements: T...) {
-       self.init(elements)
-   }
-
-   public var allObjects: [T] {
-       return objects.allObjects
-   }
-
-   public var count: Int {
-       return objects.count
-   }
-
-   public func contains(_ object: T) -> Bool {
-       return objects.contains(object)
-   }
-
-   public func insert(_ object: T) {
-       objects.add(object)
-   }
-
-   public func remove(_ object: T) {
-       objects.remove(object)
-   }
-
-   public func makeIterator() -> AnyIterator<T> {
-       let iterator = objects.objectEnumerator()
-       return AnyIterator {
-           return iterator.nextObject() as? T
-       }
-   }
-
-   public var description: String {
-       return objects.description
-   }
-
-   public var debugDescription: String {
-       return objects.debugDescription
-   }
 }
